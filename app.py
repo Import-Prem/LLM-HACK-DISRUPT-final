8503252328e7321640a89e53695e456675ab1ac7
Premkumar khemani <khemaniprem98@gmail.com>
2023-10-14 09:45:45 +0000

    changed streamlit

--- app.py
+++ app.py
@@ -17,6 +17,23 @@ from prompt_template import *
 
 import streamlit as st 
 
+# Set the page title
+st.set_page_config(
+    page_title="One Short Generation",page_icon="📸"
+    #layout="wide",
+)
+
+ 
+# Add a title to your application
+st.title("One Shot Video Generation 📸")
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 def complete_pipeline_function(topic, goal, unique_id): 
 
     ## temporary folder name
@@ -76,19 +93,38 @@ def complete_pipeline_function(topic, goal, unique_id):
 
 
 
-topic = st.text_input(label = "enter your topic")
-description =  st.text_input(label = "enter the desription") 
+topic = st.text_input(label = "Enter your topic:")
+description =  st.text_input(label = "Enter the desription:") 
 
 
 unique_id = 1234 
+# Define the desired width (you can choose any value, it will maintain a 1:1 aspect ratio)
+video_width = 400
 
-if st.button("submit"): 
-    with st.spinner("video is generating...."): 
+# Apply custom CSS to resize the video container while maintaining the aspect ratio
+st.markdown(
+    f"""
+    <style>
+    .stVideo > div > div {{
+        width: {video_width}px !important;
+        height: {video_width}px !important;
+        padding-top: 0 !important;
+        padding-bottom: 0 !important;
+        overflow: hidden !important;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+if st.button("Submit"): 
+    with st.spinner("Video is generating...."): 
+
         output = complete_pipeline_function(topic, description, unique_id)
 
         video_file = open(output, 'rb')
         video_bytes = video_file.read()
 
-
+        st.balloons()
         st.video(video_bytes)
 
